f4d81bce0baeee3aec6938366924d54199a72fe8
Danna <damalopu@gmail.com>
2025-04-06 22:15:56 -0500

    cambios a variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,13 @@
-
+import * as dotenv from 'dotenv';
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../formulario-la-salle.json");
-
+dotenv.config({ path: 'develop.env' });
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 export const db = admin.firestore();
