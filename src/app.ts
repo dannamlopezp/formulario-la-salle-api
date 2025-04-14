8e68a8b7b0dd1dfd816a71941a03208f362f4b9f
Danna <damalopu@gmail.com>
2025-04-13 21:51:28 -0500

    allow origin

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ import './config/firebase'; // inicializa Firestore
 const app = express();
 app.use(express.json());
 app.use('/api', router);
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
