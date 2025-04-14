b644433cb2b173e0e6aeb340736e5bf9232db02b
Danna <damalopu@gmail.com>
2025-04-13 21:40:51 -0500

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './interfaces/routes';
 import './config/firebase'; // inicializa Firestore
 
 const app = express();
 app.use(express.json());
 app.use('/api', router);
-
+app.use(cors());
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
