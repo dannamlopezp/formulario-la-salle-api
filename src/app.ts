ea26374081e075343fb5381d5c6948517aa5c458
Danna <damalopu@gmail.com>
2025-04-14 21:49:28 -0500

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,12 @@ import './config/firebase'; // inicializa Firestore
 const app = express();
 app.use(express.json());
 app.use('/api', router);
-const whiteList = ['http://localhost:4200/', '']
+const whiteList = ['http://localhost:4200', 'https://formulario-la-salle.firebaseapp.com']
 app.use(cors({
     origin: whiteList,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['X-Custom-Header', 'Authorization']
   }));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
