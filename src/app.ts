843de94b55b693c2b35d8f466146db7a1a5557b1
Danna <damalopu@gmail.com>
2025-04-14 22:24:32 -0500

    change import

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-import cors from 'cors';
+const cors = require('cors');
 import { router } from './interfaces/routes';
 import './config/firebase'; // inicializa Firestore
 
 const app = express();
-const whiteList = ['http://localhost:4200', 'https://formulario-la-salle.firebaseapp.com']
+const whiteList = ['http://localhost:4200', 'https://formulario-la-salle.firebaseapp.com', 'https://formulario-la-salle.web.app']
 app.use(cors({
     origin: whiteList,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['X-Custom-Header', 'Authorization']
 }));
+
 app.use(express.json());
 app.use('/api', router);
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {console.log(`Server running on port ${port}`)});
